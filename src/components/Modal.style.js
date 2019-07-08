87d3ee61986ea3abf2a1242a76c3853aca58589f
Zsolt Meszaros <zsolt@sowad.com>
2019-07-08 16:33:36 +0100

    Add Modal dialog

---
+++ src/components/Modal.style.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import { DialogContent, DialogOverlay } from '@reach/dialog';
+
+export const Content = styled(DialogContent)`
+  align-items: center;
+  background-color: #fff;
+  box-shadow: 0 1rem 3rem rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: auto;
+  max-width: 420px;
+  outline: none;
+  padding: 2rem 1rem;
+  position: relative;
+  width: 90%;
+`;
+
+export const Overlay = styled(DialogOverlay)`
+  background-color: hsla(325, 35%, 55%);
+  bottom: 0;
+  display: flex;
+  left: 0;
+  overflow: auto;
+  position: fixed;
+  right: 0;
+  top: 0;
+`;
